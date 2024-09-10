bf79d88768f085ab50f849f6e5ccaa53902ee164
islomalarov <islomalarov@gmail.com>
2024-09-10 15:16:29 +0500

    init: task-2 done

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -1,1 +1,4 @@
 // code
+let randomNumber = Math.floor(Math.random() * 100)
+randomNumber % 2 === 0 ? console.log(randomNumber, ':Это четное число') : console.log(randomNumber, 'Это нечетное число');
+
